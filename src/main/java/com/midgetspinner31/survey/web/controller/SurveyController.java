ad86cacc56073e4d3d18c390d789bea695dbe7f6
ainlorn <ap4khomov@gmail.com>
2023-11-13 23:20:46 +0500

    add javadoc

--- src/main/java/com/midgetspinner31/survey/web/controller/SurveyController.java
+++ src/main/java/com/midgetspinner31/survey/web/controller/SurveyController.java
@@ -24,16 +24,26 @@ public class SurveyController {
     SurveyService surveyService;
     SurveyAnswerService surveyAnswerService;
 
+    /**
+     * Cохранить опрос
+     */
     @PostMapping("/surveys")
     public SurveyResponse saveSurvey(@RequestBody SurveyRequest surveyRequest) {
         return new SurveyResponse(surveyService.saveSurvey(surveyRequest));
     }
 
+    /**
+     * Получить информацию об опросе
+     * @param surveyId id опроса
+     */
     @GetMapping("/surveys/{surveyId}")
     public SurveyResponse getSurvey(@PathVariable String surveyId) {
         return new SurveyResponse(surveyService.getSurvey(surveyId));
     }
 
+    /**
+     * Получить список доступных опросов
+     */
     @GetMapping("/surveys")
     public List<SurveyResponse> getSurveyList() {
         return surveyService.getSurveyList().stream()
@@ -41,17 +51,30 @@ public class SurveyController {
                 .toList();
     }
 
+    /**
+     * Удалить опрос
+     * @param surveyId id опроса
+     */
     @DeleteMapping("/surveys/{surveyId}")
     public ResponseEntity<String> deleteSurvey(@PathVariable String surveyId) {
         return ResponseEntity.ok(surveyService.deleteSurvey(surveyId));
     }
 
+    /**
+     * Отправить ответ на опрос
+     * @param surveyId id опроса
+     */
     @PostMapping("/surveys/{surveyId}/answers")
     public SurveyAnswerResponse saveSurveyAnswer(@PathVariable String surveyId,
                                                  @RequestBody @Valid SurveyAnswerRequest surveyAnswerRequest) {
         return new SurveyAnswerResponse(surveyAnswerService.saveSurveyAnswer(surveyId, surveyAnswerRequest));
     }
 
+    /**
+     * Получить информацию об ответе на опрос
+     * @param surveyId id опроса
+     * @param answerId id ответа
+     */
     @GetMapping("/surveys/{surveyId}/answers/{answerId}")
     public SurveyAnswerResponse getSurveyAnswer(@PathVariable String surveyId, @PathVariable String answerId) {
         return new SurveyAnswerResponse(surveyAnswerService.getSurveyAnswer(surveyId, answerId));
